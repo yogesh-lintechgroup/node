c8330fd386c59aa5d6fe468d98f9948589768d89
Yogesh <yogesh@lintechgroup.co.uk>
2023-09-01 12:03:10 +0530

    Variable LINTECH

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express ();
 
 app.get('/', (req, res) => {
-    res.send ("Welcome to the home page");
+    res.send ("Welcome to the home page LINTECHGROUP");
 });
 
 const port = process.env.port || 3000;
