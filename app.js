6b4fa8974ea2ae76e7e096536437b1647d0a398d
Yogesh <yogesh@lintechgroup.co.uk>
2023-09-01 14:02:05 +0530

    Commit after 2PM IST

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express ();
 
 app.get('/', (req, res) => {
-    res.send ("Welcome to my home page om 01-Sep-2023");
+    res.send ("Welcome to my home page on 01-Sep-2023");
 });
 
 const port = process.env.port || 3000;
