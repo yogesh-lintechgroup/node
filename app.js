ff8e085c9d723f578aebb3090988dfde5e3228b8
Yogesh <yogesh@lintechgroup.co.uk>
2023-09-01 11:57:34 +0530

    Variable

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express ();
 
 app.get('/', (req, res) => {
-    res.send ("Welcome to the home page HARSHU");
+    res.send ("Welcome to the home page");
 });
 
 const port = process.env.port || 3000;
